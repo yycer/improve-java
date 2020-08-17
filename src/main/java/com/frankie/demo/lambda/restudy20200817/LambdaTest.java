f9aec9e746da1f4dad170fa8f713f815bcbe8a48
yycer <15900617310@163.com>
2020-08-17 14:07:59 +0800

    Play Lambda variables.

--- src/main/java/com/frankie/demo/lambda/restudy20200817/LambdaTest.java
+++ src/main/java/com/frankie/demo/lambda/restudy20200817/LambdaTest.java
@@ -10,7 +10,23 @@ public class LambdaTest {
         System.out.println(runnable.getClass().getCanonicalName());
     }
 
+    /**
+     * 1. 非final局部变量
+     * 2. final局部变量
+     * 3. 外部类的成员变量
+     */
+    static int n3 = 30;
     public static void main(String[] args) {
-        run(() -> {});
+        int n1 = 10;
+        final int n2 = 20;
+        Runnable t1 = () -> {
+            int cur1 = n1 + 10;
+            int cur2 = n2 + 10;
+            int cur3 = n3 + 10;
+            System.out.println(cur1);
+            System.out.println(cur2);
+            System.out.println(cur3);
+            System.out.println("aa");
+        };
     }
 }
